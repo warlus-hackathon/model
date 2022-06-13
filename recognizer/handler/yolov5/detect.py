de2848d079022a93ddeb30e6956de07e93e10f4a
RomanZorkin <zorkin.roman@gmail.com>
2022-06-13 22:55:41 +0700

    chore: linter control

--- recognizer/handler/yolov5/detect.py
+++ recognizer/handler/yolov5/detect.py
@@ -38,12 +38,12 @@ if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
-from models.common import DetectMultiBackend
-from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams
-from utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
-                           increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
-from utils.plots import Annotator, colors, save_one_box
-from utils.torch_utils import select_device, time_sync
+from models.common import DetectMultiBackend  # type: ignore
+from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams  # type: ignore
+from utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,  # type: ignore
+                           increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)  # type: ignore
+from utils.plots import Annotator, colors, save_one_box  # type: ignore
+from utils.torch_utils import select_device, time_sync  # type: ignore
 
 
 @torch.no_grad()
