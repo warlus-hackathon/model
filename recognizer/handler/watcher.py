154706a6937ceda701b6882c922482e4ea65cd60
RomanZorkin <zorkin.roman@gmail.com>
2022-06-13 18:19:35 +0700

    feature: implement service to draw image with labels

--- recognizer/handler/watcher.py
+++ recognizer/handler/watcher.py
@@ -1,20 +1,48 @@
 from pathlib import Path
+import shutil
 
+import cv2
 from PIL import Image
 
 from recognizer.handler.yolov5 import detect
 
 
 model = 'recognizer/handler/yolov5/models/best.pt'
-image = Path('recognizer/file_storage/17.jpg')
+image = Path('recognizer/file_storage/33.jpg')
 file_storage = 'recognizer/file_storage'
 
 
-def read_labels():
-    pass
+def read_labels(labels: Path)-> list[str]:
+    with open(labels, 'r') as label:
+        coord = label.readlines()
+
+    boxes = []
+    for box in coord:
+        box = box.replace('\n', '')
+        box = box.split()
+        boxes.append(box)
+    labels_dir = labels.parent.parent
+    shutil.rmtree(labels_dir, ignore_errors=True)
+    return boxes
 
 
-def get_number(image_path: Path = image) -> int:     
+def write_image(image_path: Path, boxes: list):
+    image = cv2.imread(str(image_path))
+    height, width, _ = image.shape
+
+    print(height, width)
+    for box in boxes:
+        x = int(float(box[1]) * width)
+        y = int(float(box[2]) * height)
+        cv2.circle(image, (x, y), 3, (0, 255, 0), 5)
+
+    msg = f'Warlus number = {len(boxes)}'
+    cv2.putText(image, msg, (50, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 10, cv2.LINE_AA)
+    new_image = Path(image_path.parent, f'{image_path.stem}_yolov3{image_path.suffix}')
+    cv2.imwrite(new_image, image)
+
+
+def get_number(image_path: Path = image) -> int:
     im = Image.open(str(image_path))
     detect.run(
         weights=model,
@@ -25,7 +53,9 @@ def get_number(image_path: Path = image) -> int:
         save_conf=True,
         project=file_storage,
     )
-
-    return 3
+    labels = Path(file_storage, 'exp/labels', f'{image_path.stem}.txt')
+    boxes = read_labels(labels)
+    write_image(image_path, boxes)
+    return len(boxes)
 
 get_number()
