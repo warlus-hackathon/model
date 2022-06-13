fbeac46fb0d9bb14f7683c369d7b4c02f3fae924
RomanZorkin <zorkin.roman@gmail.com>
2022-06-13 16:37:11 +0700

    feature: update path to save yolo results

--- recognizer/handler/watcher.py
+++ recognizer/handler/watcher.py
@@ -1,14 +1,31 @@
 from pathlib import Path
 
+from PIL import Image
+
 from recognizer.handler.yolov5 import detect
 
 
 model = 'recognizer/handler/yolov5/models/best.pt'
 image = Path('recognizer/file_storage/17.jpg')
+file_storage = 'recognizer/file_storage'
 
 
-def get_number(image_path: Path = image) -> int:
-    detect.run(weights=model, source=str(image_path), nosave=True, save_txt=True, save_conf=True)
+def read_labels():
+    pass
+
+
+def get_number(image_path: Path = image) -> int:     
+    im = Image.open(str(image_path))
+    detect.run(
+        weights=model,
+        source=str(image_path),
+        imgsz=im.size,
+        nosave=True,
+        save_txt=True,
+        save_conf=True,
+        project=file_storage,
+    )
+
     return 3
 
 get_number()
