aed0e188b60556e9aa80b2e297383ab2f25e57f6
RomanZorkin <zorkin.roman@gmail.com>
2022-06-21 06:37:29 +0700

    chore: all hosts adresses added to env file

--- recognizer/aws.py
+++ recognizer/aws.py
@@ -6,7 +6,7 @@ session = boto3.session.Session()
 
 s3 = session.client(
     service_name='s3',
-    endpoint_url='http://127.0.0.1:9000',
+    endpoint_url=config.aws.endpoint,
     aws_access_key_id=config.aws.key_id,
     aws_secret_access_key=config.aws.key,
 )
